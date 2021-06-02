0f93d31fdf725e0916cc766ef3f45af2c146aab8
Matthias <xmatthias@outlook.com>
2021-06-02 09:05:27 +0200

    Change sequence in process

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -153,8 +153,8 @@ class FreqtradeBot(LoggingMixin):
         """
 
         # Check whether markets have to be reloaded and reload them when it's needed
-        self.exchange.reload_markets()
         loop = asyncio.get_event_loop()
+        self.exchange.reload_markets()
 
         loop.run_until_complete(self.update_closed_trades_without_assigned_fees())
 
