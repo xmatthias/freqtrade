c2b5421aa4162255bcf0bc9c12e853b69c06a326
Matthias <xmatthias@outlook.com>
2022-06-15 19:44:49 +0200

    Fix async hyperopt

--- freqtrade/optimize/hyperopt.py
+++ freqtrade/optimize/hyperopt.py
@@ -469,7 +469,7 @@ class Hyperopt:
         self.backtesting.exchange._api_async = None
         self.backtesting.exchange.loop = None  # type: ignore
         self.backtesting.exchange._loop_lock = None  # type: ignore
-        # self.backtesting.exchange = None  # type: ignore
+        self.backtesting.loop = None  # type: ignore
         self.backtesting.pairlists._loop = None  # type: ignore
         self.backtesting.dataprovider._loop = None  # type: ignore
 
